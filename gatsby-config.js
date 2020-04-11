2e3f151634d2a360b7e48ad6c81793814b5c3ef5
Vasco Marques <hello@byvasco.com>
2020-04-11 18:15:03 +0100

    Adds 404 and GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,20 @@ module.exports = {
         display: 'swap'
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-130700053-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/localhost:8000/**"],
+      },
+    }
   ]
 }
