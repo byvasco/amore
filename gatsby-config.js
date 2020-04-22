e05de890aaa5c82005d1f170bcdef97104a3ed7c
Vasco Marques <hello@byvasco.com>
2020-04-22 01:14:51 +0100

    Adds NetlifyCMS config

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
